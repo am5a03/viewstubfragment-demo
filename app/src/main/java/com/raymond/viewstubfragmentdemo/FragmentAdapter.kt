536d29cf2a4559b25d5c8c0e41ce8a587399007c
Raymond Chan <raymond@9gag.com>
2020-06-23 22:16:42 +0800

    #3 - Extending correct PagerAdapter

--- app/src/main/java/com/raymond/viewstubfragmentdemo/FragmentAdapter.kt
+++ app/src/main/java/com/raymond/viewstubfragmentdemo/FragmentAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
-class FragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+class FragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     private val titles = arrayOf("Title 1", "Title 2", "Title 3")
 
@@ -17,11 +17,7 @@ class FragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
         return f
     }
 
-    override fun getCount(): Int {
-        return 3
-    }
+    override fun getCount(): Int = titles.size
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return titles[position]
-    }
+    override fun getPageTitle(position: Int): CharSequence? = titles[position]
 }
